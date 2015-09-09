94550440adcdaf50407445b7a4f29585b8d81a9a
unknown <andrecrysthyan@hotmail.com>
2015-09-09 11:38:20 -0300

    contador follow

--- traffictcc/client/routes/user.js
+++ traffictcc/client/routes/user.js
@@ -4,12 +4,13 @@ Router.route("/user/:_id", function() {
 			var _id = this.params._id; 
 			var isFollowing = Friendships.isFollowing(_id); 
 			Session.set("currentUserId", _id); 
-			Session.set("isFollowing", isFollowing); 
-			return { 
-				user: Meteor.users.findOne({_id: _id}), 
-				posts: Posts.list(_id) 
-			}
-	} 
+			Session.set("isFollowing", isFollowing);
+return { 
+	user: Meteor.users.findOne({_id: _id}), 
+	posts: Posts.list(_id), 
+	followers: Friendships.followers(_id), 
+	followings: Friendships.followings(_id) }
+} 
 }); 
 }, { 
 	name: "user" });
