3375740232f4fa7f89dbead65897cca19c6dd922
unknown <andrecrysthyan@hotmail.com>
2015-09-09 09:57:37 -0300

    implementando follow

---
+++ traffictcc/models/friendships.js
@@ -0,0 +1,19 @@
+Friendships = new Mongo.Collection("friendships");
+Friendships.follow = function(friendId) { 
+	this.insert({ 
+		userId: Meteor.userId(), 
+		friendId: friendId 
+	}); 
+}; 
+Friendships.unfollow = function(friendId) { 
+	this.remove({ 
+		userId: Meteor.userId(), 
+		friendId: friendId 
+	}); 
+}; 
+Friendships.isFollowing = function(friendId) { 
+	return this.findOne({ 
+		userId: Meteor.userId(), 
+		friendId: friendId 
+	}); 
+};
